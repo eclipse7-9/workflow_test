9b2dfe1c5af5cfc0f948cc052371cf875d179520
eclipse7-9 <sanzspool@gmail.com>
2025-08-13 17:25:38 -0500

    Corrige config para React ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,29 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "@typescript-eslint/eslint-plugin";
-import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
+    plugins: {
+      "@eslint/js": js,
+    },
+    extends: ["plugin:@eslint/js/recommended"],
     languageOptions: {
       globals: globals.browser,
     },
   },
+
   tseslint.configs.recommended,
-  pluginReact.configs.recommended,
+
+  {
+    files: ["**/*.{jsx,tsx}"],
+    extends: ["plugin:react/recommended"],
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
 ]);

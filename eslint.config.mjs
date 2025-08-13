b552cdc82dbcea7bc43c17e1a32351212c5bce3b
eclipse7-9 <sanzspool@gmail.com>
2025-08-13 17:12:52 -0500

    Prueba para activar workflow de una vez por todas

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import pluginReact from "@typescript-eslint/eslint-plugin";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
